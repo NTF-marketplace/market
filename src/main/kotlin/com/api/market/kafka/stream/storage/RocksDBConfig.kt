9ed1ce95581ada3b817e4c4278f6af857cff29cc
min-96 <ajdcnd131@naver.com>
2024-07-23 18:16:25 +0900

    fix rocksDB ttl 30day

--- src/main/kotlin/com/api/market/kafka/stream/storage/RocksDBConfig.kt
+++ src/main/kotlin/com/api/market/kafka/stream/storage/RocksDBConfig.kt
@@ -5,7 +5,7 @@ import org.rocksdb.Options
 
 class RocksDBConfig : RocksDBConfigSetter {
     override fun setConfig(name: String, options: Options, configs: MutableMap<String, Any>) {
-        options.setTtl(3600 * 24 * 1)
+        options.setTtl(3600 * 24 * 30)
     }
 
     override fun close(name: String, options: Options) {
